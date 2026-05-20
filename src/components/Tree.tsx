9e0e6ff3779b6358f5fa57362671bf8b0b0d8230
Jahid Khan <jahidrhps123@gmail.com>
2026-05-21 00:23:02 +0530

    compressed model

--- src/components/Tree.tsx
+++ src/components/Tree.tsx
@@ -4,36 +4,27 @@ export function Tree(props:any) {
   const { nodes, materials }:{
     nodes:any,
     materials:any
-  } = useGLTF('/autumn_tree.glb')
+  } = useGLTF('/autumn_tree_compressed.glb')
   // } = useGLTF('/tree.glb')
   return (
-    // <group {...props} dispose={null}>
-    //   <mesh
-    //     castShadow
-    //     receiveShadow
-    //     geometry={nodes.mesh_0.geometry}
-    //     material={materials.branches}
-    //   />
-    //   <mesh castShadow receiveShadow geometry={nodes.mesh_1.geometry} material={materials.leaves} />
-    // </group>
-     <group {...props} dispose={null}>
-      <group scale={0.1}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Mesh_0.geometry}
-          material={materials.branches}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Mesh_1.geometry}
-          material={materials.leaves}
-        />
-      </group>
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Mesh_0.geometry}
+        material={materials.branches}
+        scale={0.1}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Mesh_1.geometry}
+        material={materials.leaves}
+        scale={0.1}
+      />
     </group>
   )
 }
 
-useGLTF.preload('/autumn_tree.glb')
+useGLTF.preload('/autumn_tree_compressed.glb')
 // useGLTF.preload('/tree.glb')
